03b69fa5ae258976b48d487d3ac3537c098118cc
Hugo Zammit <hugo.zammit@signature-code.com>
2020-07-07 13:28:00 +0200

    Add MarkovCahins Algortihms.

---
+++ ITI.RecommanderSystem.MarkovChains/TravelingSalesmanProblem.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuickGraph;
+
+namespace ITI.RecommanderSystem.MarkovChains
+{
+    public static class TravelingSalesmanProblem
+    {
+        public static IReadOnlyCollection<TVertex> Resolve<TVertex, TEdge>
+        (
+            BidirectionalGraph<TVertex, TEdge> graph,
+            IDictionary<TEdge, int> costs
+        )
+            where TEdge : IEdge<TVertex>
+        {
+            const float T0 = 10.0F;
+            const float TMin = 1e-2F;
+            const float tau = 1e4F;
+
+            var random = new Random();
+
+            int CostFunction( IReadOnlyList<TVertex> path )
+            {
+                var cost = 0;
+
+                for( var i = 1; i < path.Count; ++i )
+                {
+                    if( !graph.TryGetEdge( path[ i - 1 ], path[ i ], out var edge )
+                     && !graph.TryGetEdge( path[ i ], path[ i - 1 ], out edge ) )
+                        throw new InvalidOperationException( "graph is not complete" );
+
+                    if( !costs.TryGetValue( edge, out var edgeCost ) )
+                        throw new InvalidOperationException( "edge cost is unknown" );
+
+                    cost += edgeCost;
+                }
+
+                return cost;
+            }
+
+            var path = graph.Vertices.ToArray();
+            var EC = CostFunction( path );
+
+            var t = 0;
+            var T = T0;
+            while( T > TMin )
+            {
+                var i = random.Next( 0, graph.VertexCount );
+                var j = random.Next( 0, graph.VertexCount );
+
+                ( path[ i ], path[ j ] ) = ( path[ j ], path[ i ] );
+                var EF = CostFunction( path );
+
+                int Metropolis( int E1, int E2 )
+                {
+                    if( E1 <= E2 )
+                    {
+                        E2 = E1;
+                    }
+                    else
+                    {
+                        var dE = E1 - E2;
+                        var p = (float) random.NextDouble();
+
+                        if( p > MathF.Exp( -dE / T ) )
+                        {
+                            ( path[ i ], path[ j ] ) = ( path[ j ], path[ i ] );
+                        }
+                        else
+                        {
+                            E2 = E1;
+                        }
+                    }
+
+                    return E2;
+                }
+
+                EC = Metropolis( EF, EC );
+
+                t += 1;
+                T = T0 * MathF.Exp( -t / tau );
+            }
+
+            return path;
+        }
+    }
+}
--- ITI.RecommanderSystem.Syllabus/Program.cs
+++ ITI.RecommanderSystem.Syllabus/Program.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using ITI.RecommanderSystem.Core;
+﻿using ITI.RecommanderSystem.Core;
+using ITI.RecommanderSystem.MarkovChains;
+using QuickGraph;
+using System;
+using System.Collections.Generic;
 
 namespace ITI.RecommanderSystem.Syllabus
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main( string[] args )
         {
-            var ratings = new[,]
+            SimpleSalesman();
+        }
+
+        private static void UserBasedSimilarities()
+        {
+            var ratings = new[ , ]
             {
                 { 4, 3, 0, 0, 5, 0 },
                 { 5, 0, 4, 0, 4, 0 },
@@ -18,12 +26,93 @@ namespace ITI.RecommanderSystem.Syllabus
             };
 
             const int userId = 1;
-            var usersSimilarities = ActorSimilarities.ComputeActorsSimilarities(ratings);
-            var similarUsers = ActorSimilarities.GetSimilarActors(userId, usersSimilarities, 2);
-            var recommendations = ActorSimilarities.GetActorBasedRecommendations(userId, ratings, similarUsers, 3);
+            var usersSimilarities = ActorSimilarities.ComputeActorsSimilarities( ratings );
+            var similarUsers = ActorSimilarities.GetSimilarActors( userId, usersSimilarities, 2 );
+            var recommendations = ActorSimilarities.GetActorBasedRecommendations( userId, ratings, similarUsers, 3 );
 
-            foreach (var (id, score) in recommendations)
-                Console.WriteLine($"{id}: {score}");
+            foreach( var (id, score) in recommendations )
+                Console.WriteLine( $"{id}: {score}" );
+        }
+
+        private static void ComputePIWithMonteCarlo()
+        {
+            string s;
+            int n;
+
+            do
+            {
+                Console.WriteLine( "Enter n (n > 0): " );
+                s = Console.ReadLine();
+            } while( !int.TryParse( s, out n ) || n <= 0 );
+
+            var random = new Random();
+
+            var c = 0;
+
+            for( var i = 0; i < n; ++i )
+            {
+                var x = random.NextDouble();
+                var y = random.NextDouble();
+
+                if( x * x + y * y <= 1 )
+                {
+                    c += 1;
+                }
+            }
+
+            var piApproximation = 4D * c / n;
+            Console.WriteLine( $"PI approximation: {piApproximation:0.######}" );
+        }
+
+        private static void SimpleSalesman()
+        {
+            var graph = new BidirectionalGraph<string, Edge<string>>( false, 5, 10 );
+
+            var edge12 = new Edge<string>( "1", "2" );
+            var edge13 = new Edge<string>( "1", "3" );
+            var edge14 = new Edge<string>( "1", "4" );
+            var edge15 = new Edge<string>( "1", "5" );
+            var edge23 = new Edge<string>( "2", "3" );
+            var edge24 = new Edge<string>( "2", "4" );
+            var edge25 = new Edge<string>( "2", "5" );
+            var edge34 = new Edge<string>( "3", "4" );
+            var edge35 = new Edge<string>( "3", "5" );
+            var edge45 = new Edge<string>( "4", "5" );
+
+            graph.AddVerticesAndEdgeRange
+            (
+                new[]
+                {
+                    edge12,
+                    edge13,
+                    edge14,
+                    edge15,
+                    edge23,
+                    edge24,
+                    edge25,
+                    edge34,
+                    edge35,
+                    edge45
+                }
+            );
+
+            var costs = new Dictionary<Edge<string>, int>( 10 )
+            {
+                [ edge12 ] = 6,
+                [ edge13 ] = 6,
+                [ edge14 ] = 1,
+                [ edge15 ] = 4,
+                [ edge23 ] = 8,
+                [ edge24 ] = 3,
+                [ edge25 ] = 5,
+                [ edge34 ] = 6,
+                [ edge35 ] = 4,
+                [ edge45 ] = 2
+            };
+
+            var result = TravelingSalesmanProblem.Resolve( graph, costs );
+
+            Console.WriteLine( $"Path: {string.Join( "->", result )}" );
         }
     }
 }
